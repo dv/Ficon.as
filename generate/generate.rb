2a2d0e9e9bde2d175c3adb1f40adba8cdff626bb
David Verhasselt <david@crowdway.com>
2012-09-30 21:38:20 +0300

    Place generated .as files in correct directory

--- generate/generate.rb
+++ generate/generate.rb
@@ -4,8 +4,8 @@ require "yaml"
 template = File.open("template.as.erb", "r") {|f| f.read}
 renderer = ERB.new(template)
 
-  File.open(title + ".as", "w") do |file|
 %w(FontAwesome IconicFill IconicStroke Entypo).each do |title|
+  File.open("../com/ficon/" + title + ".as", "w") do |file|
     data = YAML::load(File.open(title + ".yml"))
     file.write(renderer.result(binding))
   end
